e56dfe636a3c6831df3521e73d89b779ee117dd0
Arjun Syah <engkukbae@gmail.com>
2021-10-21 09:25:56 +0700

    Menambahkan file input output dengan python

---
+++ BelajarPython.py
@@ -0,0 +1,14 @@
+nama      = input ("Nama:")
+namap     = input ("Nama Panggilan:")
+ttl       = input ("Tempat dan Tanggal Lahir:")
+umur      = input ("Umur:")
+tinggal   = input ("Alamat:")
+instansi  = input ("Nama Kampus:")
+nim       = input ("NIM:")
+prodi     = input ("Prodi:")
+notelp    = input ("No telepon:")
+
+
+print ("Jadi nama anda adalah",nama,"biasa dipanggil",namap,"anda lahir di",ttl,"sekarang anda berumur",umur)
+print ("dan tinggal di",tinggal,"anda adalah seorang Mahasiswa di",instansi,"dengan NIM",nim)
+print ("dan mengambil program studi",prodi,"dengan No telepon aktif",notelp) 
